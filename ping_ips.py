cc6621315991302557d77efa6248d3729c630f8b
Joselino Santos <joselinosantosti@gmail.com>
2017-07-20 13:23:49 -0300

    Add files via upload

---
+++ ping_ips.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import MySQLdb
+import os
+db = MySQLdb.connect(host="localhost", user="root", passwd="admin", db="webcrawler")
+cursor = db.cursor()
+
+arq = open('1.txt', 'r')
+texto = arq.readlines() 
+for ips in texto :
+	response = os.system("ping -c 1 " + ips)
+	if response == 0:
+  		print ips, 'Ligado!'
+  		cursor.execute("INSERT into hosts(hos_ip,hos_site)values(%s,%s)",('111','www'))
+		db.commit()
+		db.close()
+	else:
+ 		 print ips, 'Desligado!' 
+	#print(ips) 
+arq.close()
